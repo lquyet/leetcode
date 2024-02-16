bd3b5e6bc8d2e303e29ccfcded5cd7efc4b8b412
lquyet <anhquyethgvx@gmail.com>
2024-02-16 09:00:12 +0700

    naive duplicate number with hashmap

---
+++ findTheDuplicateNumber.py
@@ -0,0 +1,15 @@
+from typing import List
+
+# Naive with hashset
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        s = set()
+        for i in nums:
+            if i not in s:
+                s.add(i)
+            else:
+                return i
+        return None
+        
