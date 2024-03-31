af16b68ed7928feb94bf0cd0d5aec7dc5e9cd909
lquyet <anhquyethgvx@gmail.com>
2024-03-31 12:08:41 +0700

    try backtrack pattern

--- subSet.py
+++ subSet.py
@@ -8,3 +8,24 @@ class Solution:
             r = helper(hnums[1:])
             return r + [a + [hnums[0]] for a in r]
         return helper(nums)
+
+# Backtracking using backtrack pattern
+class BackTrack:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.r = []
+        def gen(pos: int, cur: List[int]):
+            if pos == len(nums):
+                self.r.append(cur.copy())
+                return 
+            
+            for i in [True, False]:
+                if i:
+                    cur.append(nums[pos])
+                gen(pos + 1, cur)
+                if i:
+                    cur.pop()
+        gen(0, [])
+        return self.r
+
+t = BackTrack()
+print(t.subsets([1, 2, 3]))
