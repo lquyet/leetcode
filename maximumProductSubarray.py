4d9b4f53fcee681ed7445b2b02d201d6b1a49a70
lquyet <anhquyethgvx@gmail.com>
2024-07-28 21:27:57 +0700

    maximum product continuous subarray

---
+++ maximumProductSubarray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = float('-inf')
+        n = len(nums)
+        left = 1
+        right = 1
+
+        for i in range(n):
+            left = nums[i] * left
+            right = nums[n-1-i] * right
+
+            res = max(res, left, right)
+            if left == 0:
+                left = 1
+            if right == 0:
+                right = 1           
+
+        return res
