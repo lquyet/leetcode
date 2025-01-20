ce9fc482c9fc92cfc47f19333baa654406a2ef66
quyet.leminh <quyet.leminh@shopee.com>
2025-01-20 19:41:30 +0700

    miss a commit

--- firstCompletelyPaintedRowOrColumn.py
+++ firstCompletelyPaintedRowOrColumn.py
@@ -3,5 +3,26 @@ from typing import List
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
         store = {}
+        m = len(mat)
+        n = len(mat[0])
+        length = len(arr)
 
-        for r in ranger()
+        for i in range(m):
+            for j in range(n):
+                store[mat[i][j]] = (i, j)
+        
+        count_row = [0] * m
+        count_col = [0] * n
+
+        for i in range(length):
+            row = store[arr[i]][0]
+            col = store[arr[i]][1]
+
+            count_row[row] += 1
+            count_col[col] += 1
+
+            if count_row[row] == n or count_col[col] == m:
+                return i
+            
+        
+            
