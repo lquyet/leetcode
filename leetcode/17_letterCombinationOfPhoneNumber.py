901422f2ec0950625204d292f4cce5d09d61a2c6
lquyet <anhquyethgvx@gmail.com>
2026-01-06 17:57:47 +0700

    More efficient way

--- leetcode/17_letterCombinationOfPhoneNumber.py
+++ leetcode/17_letterCombinationOfPhoneNumber.py
@@ -2,33 +2,28 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) == 0:
-            return []
-        
         dictionary = {
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z']
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
         }
-
-        res = []
-        current = []
         n = len(digits)
 
-        def helper(current, index):
+        res = []
+
+        def helper(index, current_combination):
             if index == n:
-                res.append(''.join(current))
-                return 
-            
+                res.append(current_combination)
+                return
+
             for c in dictionary[digits[index]]:
-                current.append(c)
-                helper(current, index + 1)
-                current.pop()
+                helper(index+1, current_combination + c)
+
+        helper(0, "")
         
-        helper(current, 0)
         return res
